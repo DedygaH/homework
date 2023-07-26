f2613317091fd3b005f17e0c86147ae0edbc82d4
Boris Toptunenko <131958987+DedygaH@users.noreply.github.com>
2023-07-26 15:35:16 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,25 @@
 
 
 //task 1
-function compact(array) {// оголосили функцію
-  const newArr = [];//створили порожній масив
+
+
+
+function compact(array) {                     // оголосили функцію
+  const newArr = [];                          //створили порожній масив
   
-  for (let i = 0; i < array.length; i++) {//перебераем кожен элемент
-    if (!newArr.includes(array[i])) {//Якщо елемент ще не міститься 
-// в newArr за допомогою методу includes
-      newArr.push(array[i]);//додаємо його до масиву newArr
+  for (let i = 0; i < array.length; i++) {    //перебераем кожен элемент
+    if (!newArr.includes(array[i])) {         //Якщо елемент ще не міститься 
+                                              // в newArr за допомогою методу includes
+      newArr.push(array[i]);                  //додаємо його до масиву newArr
     }
   }
   
-  return newArr;//повертається
+  return newArr;                               //повертається
 }
+
+//task 2
+//task 3
+//task 4
+//task 5
+//task 6
+//task 7
