b23946d1bb295c650195c7a14311db7172859967
Boris Toptunenko <131958987+DedygaH@users.noreply.github.com>
2023-07-26 16:41:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,21 +3,29 @@
 
 
 //task 1
+// Написати функцію compact() яка має приймати на вхід 
+// масив, а на вихід має повертати значення нового масиву без повторень.
+// приклад:
 
+// const arr = [5, 3, 4, 5,6,7,3];
+// const arr2 = compact(arr);
+// console.log(arr2); // [5,3,4,6,7]
 
-
-function compact(array) {                     // оголосили функцію
-  const newArr = [];                          //створили порожній масив
+const arr = [5, 3, 4, 5,6,7,3];
+function compact(arr) {                     // оголосили функцію
+  const arr2 = [];                          //створили порожній масив
   
-  for (let i = 0; i < array.length; i++) {    //перебераем кожен элемент
-    if (!newArr.includes(array[i])) {         //Якщо елемент ще не міститься 
-                                              // в newArr за допомогою методу includes
-      newArr.push(array[i]);                  //додаємо його до масиву newArr
+  for (let i = 0; i < arr.length; i++) {    //перебераем кожен элемент
+    if (!arr2.includes(arr[i])) {         //якщо елемент ще не міститься 
+                                              // в arr2 за допомогою методу includes
+      arr2.push(arr[i]);                  //додаємо його до масиву newArr
     }
   }
   
-  return newArr;                               //повертається
+  return arr2;                               //повертається
 }
+console.log(arr2);
+
 
 //task 2
 //task 3
